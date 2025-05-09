283634ff579a711285b4e807752566b15ab49257
anmd12345 <anvohoang98@gmail.com>
2025-05-09 22:54:58 +0700

    feat: update Firebase configuration with project-specific details

--- firebase.ts
+++ firebase.ts
@@ -2,11 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  appId: 'YOUR_APP_ID',
-  // ...
+  apiKey: "AIzaSyCx2YYUwxzpKFvnNhF2Wg-D9rnedwvlx7k",
+  authDomain: "test-firebase-app-bb07a.firebaseapp.com",
+  projectId: "test-firebase-app-bb07a",
+  storageBucket: "test-firebase-app-bb07a.appspot.com",
+  messagingSenderId: "229762496022",
+  appId: "1:229762496022:android:dd7e19ec4c6ccabdb32737"
 };
 
 const app = initializeApp(firebaseConfig);
